542f2694ea868bd8eec69b1c82dcd0be8425a0b0
beishekeevaiperi <aypei.b09@icloud.com>
2024-08-17 12:19:15 +0600

    Add files via upload

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -42,3 +42,63 @@ const moveBlock = () => {
 }
 
 moveBlock()
+
+let minutes = document.getElementById('minutes')
+let seconds = document.getElementById('seconds')
+let mlSeconds = document.getElementById('ml-seconds')
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
+const resetBtn = document.getElementById('reset')
+const resume = document.getElementById('resume')
+
+minuteS = 0
+secondS = 0
+mlSecondS = 0
+
+startBtn.onclick = function () {
+    const timerId = setInterval(function () {
+            if (mlSecondS <= 59){
+                mlSecondS++
+                mlSeconds.innerHTML = mlSecondS.toString().padStart(2, "0"); 
+            } else if ( secondS <= 59){
+                mlSecondS = 0
+                secondS++
+                seconds.innerHTML = secondS.toString().padStart(2, "0") ;
+            } else if (minuteS <= 59) { 
+                secondS = 0
+                minuteS++
+                minutes.innerHTML = minuteS.toString().padStart(2, "0");
+            }
+    }, 10)
+    stopBtn.onclick = function () {
+        clearInterval(timerId);
+    }
+    resetBtn.onclick = function () {
+        clearInterval(timerId)
+        minuteS = 0
+        secondS = 0
+        mlSecondS = 0
+        minutes.innerHTML = "00"
+        seconds.innerHTML = "00"
+        mlSeconds.innerHTML = "00"
+    }
+    resume.onclick = function () {
+        const timerId = setInterval(function () {
+            if (mlSecondS <= 59){
+                mlSecondS++
+                mlSeconds.innerHTML = mlSecondS.toString().padStart(2, "0"); 
+            } else if ( secondS <= 59){
+                mlSecondS = 0
+                secondS++
+                seconds.innerHTML = secondS.toString().padStart(2, "0") ;
+            } else if (minuteS <= 59) { 
+                secondS = 0
+                minuteS++
+                minutes.innerHTML = minuteS.toString().padStart(2, "0");
+            }
+    }, 10)
+    stopBtn.onclick = function () {
+        clearInterval(timerId);
+    }
+    }
+}
--- project/js/lesson.js
+++ project/js/lesson.js
@@ -13,3 +13,28 @@ phoneCheckt.onclick = () =>{
         phoneResult.style.color= 'red'
     }
 }
+
+//TAB SLIDER
+const tabContent = document.querySelectorAll('.tab_content_block')
+const tabs =document.querySelectorAll('.tab_content_item')
+const tabsParent =document.querySelector('.tab_content_items')
+
+
+const hideTabContent = () => {
+    tabContent.forEach( (item) => {
+        item.style.display = 'none'
+    })
+    tabs.forEach( (item) => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+
+
+const showTabContent = (index = 0) => {
+    tabContent[index].style.display = 'block'
+    tabs[index].classList.add('tab_content_item_active')
+}
+
+
+hideTabContent()
+showTabContent()
